6ac4f5618bea5959e0b78df25f1ca812ebf74b1f
changxingzhao <changxingzhao@kuaishou.com>
2022-02-14 11:08:11 +0800

    feat: regular update

--- docs/functional-programming/compose.js
+++ docs/functional-programming/compose.js
@@ -1,10 +1,56 @@
 // compose函数只能组合仅接受一个参数的函数
 // 可以使用partial将多参数函数变为单参数函数
-const compose = (a, b) => (c) =>
-    a(b(c));
+// const compose = (a, b) => (c) => a(b(c));
 
-let splitIntoSpaces = (str) => str.split(' ');
-let count = (array) => array.length;
+// let splitIntoSpaces = (str) => str.split(" ");
+// let count = (array) => array.length;
 
-const countWords = compose(count, splitIntoSpaces);
-console.log(countWords('hello world!'));
+// const countWords = compose(count, splitIntoSpaces);
+// console.log(countWords("hello world!"));
+
+function compose() {
+    for (
+        var _len = arguments.length, funcs = new Array(_len), _key = 0;
+        _key < _len;
+        _key++
+    ) {
+        funcs[_key] = arguments[_key];
+    }
+
+    if (funcs.length === 0) {
+        return function (arg) {
+            return arg;
+        };
+    }
+
+    if (funcs.length === 1) {
+        return funcs[0];
+    }
+
+    return funcs.reduce(function (accu, curr) {
+        return function () {
+            return accu(curr.apply(void 0, arguments));
+        };
+    });
+}
+
+function a(str) {
+    console.log("a");
+    return str + "a";
+}
+
+function b(str) {
+    console.log("b");
+    return str + "b";
+}
+
+function c(str) {
+    console.log("c");
+    return str + "c";
+}
+
+const res = compose(a, b, c);
+
+const str = res("x");
+
+console.log(str);
---
+++ docs/html/link.md
@@ -0,0 +1,24 @@
+# Link
+
+> The `<link>` HTML element specifies relationships between the current document and an external resource
+> most commonly used to link to stylesheets
+
+## 属性
+
+### rel (relationship)
+
+可选值：'stylesheet', 'icon'
+
+### sizes
+
+icon大小
+
+### crossorigin
+
+The `crossorigin` attribute indicates whether the resource should be fetched with a CORS request.
+
+### media
+
+the media that the linked resource applies to
+
+- 设置之后css加载可能不会阻塞html的渲染
