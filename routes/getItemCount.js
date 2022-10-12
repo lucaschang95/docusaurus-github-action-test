af339e6c264556bfeb8d7c37034e9d425d738f3d
lucaschang95 <xingzhao.chang@gmail.com>
2022-10-12 22:04:11 +0800

    test

--- routes/getItemCount.js
+++
@@ -1,6 +0,0 @@
-const db = require('../backend/src/database');
-
-module.exports = async (req, res) => {
-    const count = await db.getItemCount();
-    res.send({ count: count?.[0]?.['COUNT(*)'] ?? 0});
-};
--- routes/getItems.js
+++
@@ -1,6 +0,0 @@
-const db = require('../backend/src/database');
-
-module.exports = async (req, res) => {
-    const items = await db.getItems();
-    res.send(items);
-};
